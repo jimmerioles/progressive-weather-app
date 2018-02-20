36f504d530f04d81c0b1b93a6ba76cae74fcce7e
Jim Merioles <jimwisleymerioles@gmail.com>
2018-02-21 01:51:10 +0800

    Subpath fix for github

---
+++ precache-manifest.e61ad2a8d9af81135fe40a3ea0fcd5f7.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "e7cc8477abd5a403ccc9dba2dd4a7c00",
+    "url": "/progressive-weather-app/img/humidity.e7cc8477.svg"
+  },
+  {
+    "revision": "2ede19b093d97208f5a4",
+    "url": "/progressive-weather-app/js/vendor.2ede19b0.js"
+  },
+  {
+    "revision": "775ad4d166459c56f1d5ab6866d737c3",
+    "url": "/progressive-weather-app/img/cloud.775ad4d1.svg"
+  },
+  {
+    "revision": "7163d1b62a543e5dc82cb66b1ec59bd5",
+    "url": "/progressive-weather-app/index.html"
+  },
+  {
+    "revision": "089cd8beba94851ed228",
+    "url": "/progressive-weather-app/js/manifest.089cd8be.js"
+  },
+  {
+    "revision": "ab0006cec11183f84b03",
+    "url": "/progressive-weather-app/js/app.ab0006ce.js"
+  },
+  {
+    "revision": "05f5c4cf26ff3e58bdd441ed54d3604b",
+    "url": "/progressive-weather-app/img/wind.05f5c4cf.svg"
+  },
+  {
+    "revision": "7e644c1a17230752ab109b8b5ebc6c02",
+    "url": "/progressive-weather-app/img/thunderstorm.7e644c1a.svg"
+  },
+  {
+    "revision": "0bb8cb16f4420944c265dfc314c09bb8",
+    "url": "/progressive-weather-app/img/snow.0bb8cb16.svg"
+  },
+  {
+    "revision": "a56a791585c9caa047aff2b838dfd086",
+    "url": "/progressive-weather-app/img/rain.a56a7915.svg"
+  },
+  {
+    "revision": "f3a3ae94eefef4fe983994a3fd245510",
+    "url": "/progressive-weather-app/img/night.f3a3ae94.jpg"
+  },
+  {
+    "revision": "f9efa637a49d2652a0a94b482422dd9c",
+    "url": "/progressive-weather-app/img/morning.f9efa637.jpg"
+  },
+  {
+    "revision": "61a481cf6f19470210a9f276ed350690",
+    "url": "/progressive-weather-app/img/low.61a481cf.svg"
+  },
+  {
+    "revision": "d70bbaa2cbca8d03bb175cf58fcb08ad",
+    "url": "/progressive-weather-app/img/high.d70bbaa2.svg"
+  },
+  {
+    "revision": "2b02f907ba6575d42dc28cebb0ca2080",
+    "url": "/progressive-weather-app/img/cloud.2b02f907.svg"
+  },
+  {
+    "revision": "ab0006cec11183f84b03",
+    "url": "/progressive-weather-app/css/app.7d5a2ac8.css"
+  }
+];
