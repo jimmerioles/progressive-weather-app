5d0e189bfbcfcc6eec09fd7f256a2acd0d9b0e50
Jim Merioles <jimwisleymerioles@gmail.com>
2018-02-21 01:18:17 +0800

    Added dist

---
+++ service-worker.js
@@ -0,0 +1,28 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/YYPcyY
+ */
+
+importScripts(
+  "/precache-manifest.5b946ca32d8cac51236d3357db3b02ba.js",
+  "https://storage.googleapis.com/workbox-cdn/releases/3.0.0-beta.0/workbox-sw.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "local-weather-pwa"});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
