5d4d561cb8fad4950740bb94feb4a7d6b2740c3f
Jim Merioles <jimwisleymerioles@gmail.com>
2018-02-21 01:18:17 +0800

    Added dist

---
+++ dist/precache-manifest.5b946ca32d8cac51236d3357db3b02ba.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "e7cc8477abd5a403ccc9dba2dd4a7c00",
+    "url": "/img/humidity.e7cc8477.svg"
+  },
+  {
+    "revision": "2ede19b093d97208f5a4",
+    "url": "/js/vendor.2ede19b0.js"
+  },
+  {
+    "revision": "775ad4d166459c56f1d5ab6866d737c3",
+    "url": "/img/cloud.775ad4d1.svg"
+  },
+  {
+    "revision": "ed91c54b6423835cda3c7728e272d88c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ce28c628eea246b643c9",
+    "url": "/js/manifest.ce28c628.js"
+  },
+  {
+    "revision": "64c91643208ecbdf1675",
+    "url": "/js/app.64c91643.js"
+  },
+  {
+    "revision": "05f5c4cf26ff3e58bdd441ed54d3604b",
+    "url": "/img/wind.05f5c4cf.svg"
+  },
+  {
+    "revision": "7e644c1a17230752ab109b8b5ebc6c02",
+    "url": "/img/thunderstorm.7e644c1a.svg"
+  },
+  {
+    "revision": "0bb8cb16f4420944c265dfc314c09bb8",
+    "url": "/img/snow.0bb8cb16.svg"
+  },
+  {
+    "revision": "a56a791585c9caa047aff2b838dfd086",
+    "url": "/img/rain.a56a7915.svg"
+  },
+  {
+    "revision": "f3a3ae94eefef4fe983994a3fd245510",
+    "url": "/img/night.f3a3ae94.jpg"
+  },
+  {
+    "revision": "f9efa637a49d2652a0a94b482422dd9c",
+    "url": "/img/morning.f9efa637.jpg"
+  },
+  {
+    "revision": "61a481cf6f19470210a9f276ed350690",
+    "url": "/img/low.61a481cf.svg"
+  },
+  {
+    "revision": "d70bbaa2cbca8d03bb175cf58fcb08ad",
+    "url": "/img/high.d70bbaa2.svg"
+  },
+  {
+    "revision": "2b02f907ba6575d42dc28cebb0ca2080",
+    "url": "/img/cloud.2b02f907.svg"
+  },
+  {
+    "revision": "64c91643208ecbdf1675",
+    "url": "/css/app.e555021d.css"
+  }
+];
